ca1ed0c23f2de51964c8974167be710a66ea045d
JBee-s <saillourj@outlook.fr>
2023-07-10 16:33:21 +0200

    Exo4 v1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    colors: {
+    'Dark Grayish Magenta': 'hsl(303, 10%, 53%)',
+    'Light Grayish Magenta': 'hsl(300, 24%, 96%)',
+    'White': 'hsl(0, 0%, 100%)',
+      },
+
+}
