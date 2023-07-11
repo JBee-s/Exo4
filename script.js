e5cdb271607af4730e4ae67d9cef81e1261801f7
JBee-s <saillourj@outlook.fr>
2023-07-11 16:35:37 +0200

    Exo4 v3

---
+++ script.js
@@ -0,0 +1,11 @@
+const titre = document.querySelector("h1");
+let i = 0;
+titre.style.position = "absolute"
+
+
+function droiteGauche(){
+    
+    requestAnimationFrame(droiteGauche);
+}
+
+requestAnimationFrame(droiteGauche);
